1ad2bf59c49963ac97da8417fc14a71a5cf61238
Pradeep Yadav <pradeepyadav8253@gmail.com>
2024-08-31 15:23:03 +0530

    datatype added

---
+++ basics/datatype.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as modern javascript
+
+//alert (3 + 3 )  // we are using nodejs , not browser
+
+let name = "pradeep";
+let age = 24;
+let isLoggedIn = false;
+
+// number  => 2 to power 53
+// otherwise bigint are used
+// string  => ""
+// boolean => true or false
+// null =>  standalone value
+//symbol => unique identifier
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
