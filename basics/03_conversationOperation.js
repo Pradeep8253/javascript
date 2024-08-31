996f37a587ae0db0aa550c2c74de2dbf48d57bb5
Pradeep Yadav <pradeepyadav8253@gmail.com>
2024-08-31 15:47:40 +0530

    conversation

---
+++ basics/03_conversationOperation.js
@@ -0,0 +1,30 @@
+let score = "33";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// if score is  "33" => 33
+// "33abc" => NaN
+//  true => 1  and false => 0
+// socre = null => 0
+
+let isLoggedIn = "rjhoifush";
+
+let booleanISLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanISLoggedIn);
+
+// 1 => true ; 0 => false
+// "" =>  false
+// "hitesh" => true
+
+let number = 33;
+
+let stringNumber = String(number);
+console.log(number);
+console.log(typeof stringNumber);
