7f95ea6feaa5000e2604cf2c392956851c61bb1e
Pradeep8253 <89512667+Pradeep8253@users.noreply.github.com>
2024-09-22 00:09:44 +0530

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,17 @@
+
+var c = 300 
+let a =  80 ;
+
+if (true){
+    let  a = 10 ; 
+    const b = 20 ;
+    var c = 30 ;
+    console.log("Inner A:" , a);
+    
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+
