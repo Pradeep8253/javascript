cc1697140096c36fe0f8fe4d7b2d6ac4dc679ef8
Pradeep8253 <89512667+Pradeep8253@users.noreply.github.com>
2024-09-28 07:46:58 +0530

    for

---
+++ 05_iterations/one.js
@@ -0,0 +1,55 @@
+// FOR 
+
+// for (let i = 0 ; i<=10 ; i++){
+//     const element = i;
+//     console.log(i);
+    
+// }
+
+
+
+// for (let i = 0; i <= 10; i++) {
+
+//     console.log(`Outer loop value ${i}`);
+    
+//    for (let j = 0; j <=10 ; j++) {
+//    console.log(`Inner loop value ${j} andd inner loop ${i}`);
+   
+    
+//    }
+    
+// }
+
+
+
+let arr = ["shivam" , "pradeep" , "Abhishek" , "rajat" , "lokesh"] ;
+
+
+// for (let i = 0; i < arr.length; i++) {
+//     const element = arr[i];  
+// }
+
+
+//  BREAK AND CONTINUE 
+
+
+// for (let i = 1; i <=20 ; i++) {
+
+//     if(i == 5 ){
+//         console.log(`Detected 5`);
+//         break ;
+//     }
+// console.log(`value of index ${i}`);
+    
+// }
+
+for (let i = 1; i <=20 ; i++) {
+
+    if(i == 5 ){
+        console.log(`Detected 5`);
+        continue ;
+    }
+console.log(`value of index ${i}`);
+    
+}
+
